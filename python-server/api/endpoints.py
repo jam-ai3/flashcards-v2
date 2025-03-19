283dd89e11fcb28e6327d39b2e28160e4c6de606
Matt Linder <108966946+mlinder10@users.noreply.github.com>
2025-03-18 22:06:40 -0400

    refactored python server

---
+++ python-server/api/endpoints.py
@@ -0,0 +1,63 @@
+# pdf conversion endpoint
+from flask import jsonify, request
+from flask_cors import cross_origin
+from util.pdf_extractor import PdfParser
+from util.pptx_extractor import get_text_from_pptx
+from util.generator.gemini import generate
+
+
+class PdfConversionEndpoint:
+    def __init__(self, app):
+        self.app = app
+
+        @app.route('/pdf', methods=['POST'])
+        @cross_origin(origins="*")
+        def pdf_conversion():
+            print("Received request to convert PDF")
+            pdf_file = request.files["pdf"]
+            pdf_binary_data = pdf_file.stream.read()
+            extracted_text = ""
+            try:
+                parser = PdfParser(pdf_binary_data)
+                extracted_text = parser.get_all_text_without_chapters()
+                return jsonify(extracted_text)
+            except:
+                print("Error processing PDF file")
+                return jsonify({"error": "Failed to process PDF file"}), 500
+
+
+class PptxConversionEndpoint:
+    def __init__(self, app):
+        self.app = app
+
+        @app.route('/pptx', methods=['POST'])
+        @cross_origin(origins="*")
+        def pptx_conversion():
+            pptx_file = request.files['pptx']
+            pptx_binary_data = pptx_file.stream.read()
+            extracted_text = ""
+            try:
+                extracted_text = get_text_from_pptx(pptx_binary_data)
+                return jsonify(extracted_text)
+            except:
+                print("Error processing PPTX file")
+                return jsonify({"error": "Failed to process PPTX file"}), 500
+
+
+class GenerateFlashcardsEndpoint:
+    def __init__(self, app):
+        self.app = app
+
+        @app.route('/generate', methods=['POST'])
+        @cross_origin(origins="*")
+        def generate_flashcards():
+            data = request.json
+            input_type = data['inputType']
+            text = data['text']
+            is_free = data['isFree']
+
+            if input_type == None or text == None or is_free == None:
+                return jsonify({"error": "Missing required fields"}), 400
+
+            flashcards = generate(input_type, text, is_free)
+            return jsonify(flashcards)
--- python-server/server.py
+++ python-server/server.py
@@ -1,10 +1,8 @@
-from util.pptx_extractor import get_text_from_pptx
-from util.pdf_extractor import PdfParser
-from flask import Flask, jsonify
-from flask import request
-from flask_cors import CORS, cross_origin
+from flask import Flask
+from flask_cors import CORS
 import os
 import sys
+from api.endpoints import *
 
 # I DON"T KNOW HOW THIS LINE BEHAVES ON VERCEL
 # It includes the util in the path so we can use it
@@ -14,38 +12,9 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
-
-@app.route('/pptx', methods=['POST'])
-@cross_origin(origins="*")
-def pptx_conversion():
-    pptx_file = request.files['pptx']
-    # CAN CAUSE MEMORY ISSUES
-    pptx_binary_data = pptx_file.stream.read()
-    extracted_text = ""
-    try:
-        extracted_text = get_text_from_pptx(pptx_binary_data)
-        return jsonify(extracted_text)
-    except:
-        print("Error processing PPTX file")
-        return jsonify({"error": "Failed to process PPTX file"}), 500
-
-
-@app.route('/pdf', methods=['POST'])
-@cross_origin(origins="*")
-def pdf_conversion():
-    print("Received request to convert PDF")
-    pdf_file = request.files["pdf"]
-    # CAN CAUSE MEMORY ISSUES
-    pdf_binary_data = pdf_file.stream.read()
-    extracted_text = ""
-    try:
-        parser = PdfParser(pdf_binary_data)
-        extracted_text = parser.get_all_text_without_chapters()
-        return jsonify(extracted_text)
-    except:
-        print("Error processing PDF file")
-        return jsonify({"error": "Failed to process PDF file"}), 500
-
+PdfConversionEndpoint(app)
+PptxConversionEndpoint(app)
+GenerateFlashcardsEndpoint(app)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
