150808bd9ff131aa1c1eb77da03cd6a738dc7b90
arshiaesll <arshia.esl83@gmail.com>
2025-03-20 19:06:55 -0400

    changed the server file

--- python-server/routes/server.py
+++ python-server/routes/server.py
@@ -2,21 +2,26 @@ from flask import Flask
 from flask_cors import CORS
 import os
 import sys
-from api.endpoints import *
 
-# I DON"T KNOW HOW THIS LINE BEHAVES ON VERCEL
-# It includes the util in the path so we can use it
+# Add parent directory to path (for util)
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# Add api directory to path
+sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'api'))
 
+from api.endpoints import *
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
+
+
+@app.route("/")
+def hello_world():
+    return "hello world!"
 
 PdfConversionEndpoint(app)
 PptxConversionEndpoint(app)
 GenerateFlashcardsEndpoint(app)
 
-# app = app.wsgi_app
 wsgi_app = app
-# if __name__ == "__main__":
-#     app.run(debug=True, port=5000)
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
