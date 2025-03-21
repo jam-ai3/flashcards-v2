1ffebcd3eb8c924c27463648923ddffbbd059aff
EC2 Default User <ec2-user@ip-172-31-5-140.us-east-2.compute.internal>
2025-03-21 01:09:54 +0000

    adding changes

--- python-server/app.py
+++ python-server/app.py
@@ -25,4 +25,4 @@ GenerateFlashcardsEndpoint(app)
 # app = app.wsgi_app
 # wsgi_app = app
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(host = '0.0.0.0', debug=True, port=80)
